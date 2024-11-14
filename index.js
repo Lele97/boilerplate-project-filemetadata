6e8f53b7f3fa4c68210408a4675ae2c810753dbe
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2024-11-14 22:21:23 +0000

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*const express = require('express');
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const multer = require('multer');
@@ -63,39 +63,4 @@ app.post('/api/fileanalyse', (req, res) => {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port);
-});
-*/
-const express = require("express"),
-  cors = require("cors"),
-  multer = require("multer"), 
-  // require and use "multer"... https://www.npmjs.com/package/multer
-  upload = multer({ dest: "uploads/" });
-
-
-const app = express();
-
-app.use(cors());
-app.use("/public", express.static(process.cwd() + "/public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
-
-app.get("/hello", (req, res) => {
-  res.json({ greetings: "Hello, API" });
-});
-
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
-  
-  if (!req.file.originalname) {
-    return res.send("Something went wrong");
-  }
-  const { originalname, mimetype, size } = req.file;
-  const fileMetaData = { name: originalname, type: mimetype, size };
-  next();
-  return res.send(fileMetaData);
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Node.js listening ...");
 });
